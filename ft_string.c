b20289dc38b53de440039b718e3a00eeb7c3da46
Zhangfeyy <zhangfeyy@outlook.com>
2025-05-05 21:52:07 +0200

    changed

--- ft_string.c
+++ ft_string.c
@@ -1,30 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_string.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fzhang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/05 21:36:51 by fzhang            #+#    #+#             */
-/*   Updated: 2025/05/05 21:36:52 by fzhang           ###   ########.fr       */
+/*   Created: 2025/05/05 21:50:18 by fzhang            #+#    #+#             */
+/*   Updated: 2025/05/05 21:50:21 by fzhang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-int	ft_printf(const char *, ...)
-{
-	
-}
-
-// check the derected descriptor, and keep the other ones
-type	ft_check(const char * string)
-{
-	
-}
-//go through every character, and fill in the blank space by specific character
-int ft_write()
-{
-
-
-}
-//
